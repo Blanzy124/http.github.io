fcecf882e08aa9679c888ccecf358c8d0afc0be8
Blanzy124 <samuelmedinablandon124@gmail.com>
2024-12-04 22:11:20 -0800

    learning

--- http.mjs
+++ http.mjs
@@ -1,11 +1,25 @@
 import http from 'node:http';
+import fs from 'node:fs'
 
 const server = http.createServer((req, res) => {
  console.log(`request resivida`)
  if (req.url === '/'){
   res.statusCode = 200
   res.setHeader('Content-Type', 'text/plane; charset=utf-8')
-  res.end('<h1>HOLA QUE TAL</h1>')
+  res.end('HOLA QUE TAL')
+ }
+ else if (req.url === '/senna'){
+  fs.readFile('./images_test/ssenna.png', (err, ima) => {
+   if (err) {
+    res.statusCode = 500
+    res.end('algo salio mal')
+   }
+   else {
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'image/png')
+    res.end(ima)
+   }
+  })
  }
 })
  
@@ -14,6 +28,5 @@ const server = http.createServer((req, res) => {
 server.listen(0, () => {
  console.log(`se escucha el puerto http://localhost:${server.address().port}`)
 })
-
 
 
