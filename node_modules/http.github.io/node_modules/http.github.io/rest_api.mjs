04e4747cb8ae67ecc9424cab3e823c895c2db2dc
Blanzy124 <samuelmedinablandon124@gmail.com>
2024-12-08 23:21:10 -0800

    seguimos

--- node_modules/http.github.io/node_modules/http.github.io/rest_api.mjs
+++ node_modules/http.github.io/node_modules/http.github.io/rest_api.mjs
@@ -1,5 +1,5 @@
 import express from 'express';
-import fs from 'node:fs'
+import fs from 'node:fs';
 import coments from './users/coments.json'  assert { type: 'json' };
 
 const app = express()
@@ -8,10 +8,34 @@ const PORT = process.env.PORT ?? 1235
 
 
 
-app.get('/', (req, res ) => {
+app.get('/coments', (req, res ) => {
+ const { name } = req.query
+ if (name) {
+  const filterUserComents = coments.filter(
+   coment => coment.name.toLowerCase().includes(name.toLowerCase())
+  )  
+  return res.json(filterUserComents)
+ }
+ else{
+  console.log('peticion de todos los comentarios')
+  res.send(coments)
+ }
+ }
+) 
+
+app.get('/coments/:id', (req, res ) => {
+ const { id } = req.params; 
+ const coment = coments.find(coment => coment.id === id)
+ if (coment) return res.json(coment)
+ console.log(`request recieved id`)
+})
+
+app.get('/coments/user', (req, res ) => {
  res.send(coments)
  console.log(`request recieved`)
 })
+
+
 
 app.post('/addcoment', (req, res) => {
  let body = ''
