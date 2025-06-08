0df5ca32211a0278590cfc300e2dd21ffce7d206
Blanzy124 <samuelmedinablandon124@gmail.com>
2025-06-08 13:41:45 -0700

    bug fix deploment update

--- rest_api.mjs
+++ rest_api.mjs
@@ -24,16 +24,18 @@ app.use(express.json());
 app.use(cookieParser())
 //corsMiddleware();
 app.use((req, res, next) => {
-  const allowedOrigins = ['http://127.0.0.1:3000', 'http://localhost:8443/','https://blanzynetwork.com', 'https://152.67.231.147', 'https://www.blanzynetwork.com'];
+  const allowedOrigins = ['http://127.0.0.1:3000', 'http://localhost:8443','https://blanzynetwork.com', 'https://www.blanzynetwork.com', 'https://152.67.231.147', 'https://www.blanzynetwork.com'];
   const origin = req.headers.origin;
-
   if (allowedOrigins.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
   }
-
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+  if (req.method === 'OPTIONS') {
+  return res.sendStatus(200);
+  }
   next();
 });
 
